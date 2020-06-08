6d5c38211a64a6ae40da6e959bb8d7298f507ec3
Kelvin Jayaram <kelvin.rock.aplus@gmail.com>
2020-06-08 17:59:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  return string.toLowerCase()
-
+  return "I can\'t hear you" string.toLowerCase()
+  return "YES INDEED!" string.toUpperCase()
+  return "I love you, too" string.mixedCase()
 }
